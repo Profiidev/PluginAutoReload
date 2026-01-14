ea83567f3815be302cab397c16881622fa5bbc80
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-01-14 22:47:57 +0100

    feat: initial commit

---
+++ src/main/java/io/profidev/FirstHytaleMod.java
@@ -0,0 +1,23 @@
+package io.profidev;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class FirstHytaleMod extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public FirstHytaleMod(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("Setting up plugin " + this.getName());
+        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+    }
+}
